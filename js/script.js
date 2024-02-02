73c5a658658e95670255de7fd7fa9f81a2c9387e
SerenaMalusa <sere.malu@gmail.com>
2024-02-02 13:00:30 +0100

    feat: add filter for last received message and fix some variables name and create activeContact and activeMessages in computed

--- js/script.js
+++ js/script.js
@@ -3,179 +3,28 @@ const { createApp } = Vue;
 const app = createApp ({
     data() {
         return {
-            contacts: [
-                {
-                    name: 'Michele',
-                    avatar: './img/avatar_1.jpg',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Hai portato a spasso il cane?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Ricordati di stendere i panni',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 16:15:22',
-                            message: 'Tutto fatto!',
-                            status: 'received'
-                        }
-                    ],
-                },
-                {
-                    name: 'Fabio',
-                    avatar: './img/avatar_2.jpg',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '20/03/2020 16:30:00',
-                            message: 'Ciao come stai?',
-                            status: 'sent'
-                            },
-                        {
-                            date: '20/03/2020 16:30:55',
-                            message: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
-                        },
-                        {
-                            date: '20/03/2020 16:35:00',
-                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
-                        }
-                    ],
-                },
-                {
-                    name: 'Samuele',
-                    avatar: './img/avatar_3.jpg',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '28/03/2020 10:10:40',
-                            message: 'La Marianna va in campagna',
-                            status: 'received'
-                        },
-                        {
-                            date: '28/03/2020 10:20:10',
-                            message: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '28/03/2020 16:15:22',
-                            message: 'Ah scusa!',
-                            status: 'received'
-                        }
-                    ],
-                },
-                {
-                    name: 'Alessandro B.',
-                    avatar: './img/avatar_4.jpg',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
-                        }
-                    ],
-                },
-                {
-                    name: 'Alessandro L.',
-                    avatar: './img/avatar_5.jpg',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Ricordati di chiamare la nonna',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Va bene, stasera la sento',
-                            status: 'received'
-                        }
-                    ],
-                },
-                {
-                    name: 'Claudia',
-                    avatar: './img/avatar_6.jpg',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Ciao Claudia, hai novità?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Non ancora',
-                            status: 'received'
-                        },
-                        {
-                            date: '10/01/2020 15:51:00',
-                            message: 'Nessuna nuova, buona nuova',
-                            status: 'sent'
-                        }
-                    ],
-                },
-                {
-                    name: 'Federico',
-                    avatar: './img/avatar_7.jpg',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                            status: 'received'
-                        }
-                    ],
-                },
-                {
-                    name: 'Davide',
-                    avatar: './img/avatar_8.jpg',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                            status: 'received'
-                        },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 15:51:00',
-                            message: 'OK!!',
-                            status: 'received'
-                        }
-                    ],
-                }
-            ],
+            contacts: contacts,
             // index of the contact whose chat is open
-            activeContact: 5,
+            activeChat: 0,
         }
     },
 
     computed: {
-        
+        // always get the data of the contact with i = activChat
+        activeContact() {
+            return this.contacts[this.activeChat];
+        },
+        // messages of the active contact
+        activeMessages() {
+            return this.activeContact.messages;
+        },
     },
 
     methods: {
+        // function that changes the active contact to the index of the clicked chat
+        goToChat(index) {
+            this.activeChat = index;
+        },
         // function that gets the time from the date string
         messageTime(date) {
             return date.substr(11,5);
@@ -184,10 +33,24 @@ const app = createApp ({
         messageDate(date) {
             return date.substr(0,10);
         },
-        // function that changes the active contact to the index of the clicked chat
-        goToChat(index) {
-            this.activeContact = index;
-        }
+        // function that gets filter for the received messages and gets the last one
+        getLastReceivedMessage(messages) {
+            // filter for the received messages
+            const receivedMessages = messages.filter((message) => message.status == 'received');
+            // returns only the last message
+            return receivedMessages[receivedMessages.length - 1];
+        },
+        // print the string about the last access
+        printLastAccess() {
+            // save the last message's date
+            const lastReceivedTime = this.getLastReceivedMessage(this.activeMessages).date;
+            // returns the string to be printed in page
+            return `Ultimo accesso alle 
+                    ${this.messageTime(lastReceivedTime)} 
+                    del 
+                    ${this.messageDate(lastReceivedTime)}
+                    `
+        },
     },
 });
 
